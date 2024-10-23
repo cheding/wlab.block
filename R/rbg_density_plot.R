6c8dbe72a19b14ecc1a5d0f9dac78bcbdf6682b7
Chen Ding <chending@mail.ustc.edu.cn>
2024-10-23 15:15:23 +0800

    update

--- R/rbg_density_plot.R
+++ R/rbg_density_plot.R
@@ -30,6 +30,15 @@ rbg_density_plot<-function(
     ggplot2::facet_grid(block_plot~phenotypes_plot)
   ggplot2::ggsave(file.path(output_dir, "real_backgrounds_mean_input_count.pdf"), d, width = num+2, height = length(block)+1, useDingbats=FALSE)
 
+  plot_class<-plot_dt[,.(sum_count=sum(mean_count)),by=.(phenotype,real_bg)]
+  p <- ggplot2::ggplot(plot_class,ggplot2::aes(x = factor(phenotype),y=sum_count, fill =factor(real_bg))) +
+    ggplot2::geom_bar(stat = "identity", position = "fill") +
+    ggplot2::xlab("experiment") +
+    ggplot2::ylab("Percentage of reads with mutations") +
+    ggplot2::labs(fill=NULL)+
+    ggplot2::theme_bw()
+  ggplot2::ggsave(file.path(output_dir, "real_backgrounds_mean_input_percentage.pdf"), p, width = num+2.5, height = 4, useDingbats=FALSE)
+
   #Real doubles based on NNK
   plot_dt <- rbindlist(mc_nnk_list)
   plot_dt[, block_plot := paste0("block = ", substr(phenotype, nchar(phenotype), nchar(phenotype)))]
@@ -42,6 +51,15 @@ rbg_density_plot<-function(
     ggplot2::facet_grid(block_plot~phenotypes_plot)
   ggplot2::ggsave(file.path(output_dir, "real_nnk_doubles_mean_input_count.pdf"), d, width = num+2, height = length(block)+1, useDingbats=FALSE)
 
+  plot_class<-plot_dt[,.(sum_count=sum(mean_count)),by=.(phenotype,real_bg)]
+  p <- ggplot2::ggplot(plot_class[Nham_aa==2],ggplot2::aes(x = factor(phenotype),y=sum_count, fill =factor(real_bg))) +
+    ggplot2::geom_bar(stat = "identity", position = "fill") +
+    ggplot2::xlab("experiment") +
+    ggplot2::ylab("Percentage of reads with mutations") +
+    ggplot2::labs(fill=NULL)+
+    ggplot2::theme_bw()
+  ggplot2::ggsave(file.path(output_dir, "real_nnk_doubles_mean_input_percentage.pdf"), p, width = num+2.5, height = 4, useDingbats=FALSE)
+
   #Real singles based on NNK
   plot_dt <- rbindlist(mc_nnk_list)
   plot_dt[, block_plot := paste0("block = ", substr(phenotype, nchar(phenotype), nchar(phenotype)))]
@@ -53,4 +71,13 @@ rbg_density_plot<-function(
     ggplot2::theme_bw() +
     ggplot2::facet_grid(block_plot~phenotypes_plot)
   ggplot2::ggsave(file.path(output_dir, "real_nnk_singles_mean_input_count.pdf"), d, width = num+2, height = length(block)+1, useDingbats=FALSE)
+
+  plot_class<-plot_dt[,.(sum_count=sum(mean_count)),by=.(phenotype,real_bg)]
+  p <- ggplot2::ggplot(plot_class[Nham_aa==1],ggplot2::aes(x = factor(phenotype),y=sum_count, fill =factor(real_bg))) +
+    ggplot2::geom_bar(stat = "identity", position = "fill") +
+    ggplot2::xlab("experiment") +
+    ggplot2::ylab("Percentage of reads with mutations") +
+    ggplot2::labs(fill=NULL)+
+    ggplot2::theme_bw()
+  ggplot2::ggsave(file.path(output_dir, "real_nnk_singles_mean_input_percentage.pdf"), p, width = num+2.5, height = 4, useDingbats=FALSE)
 }
